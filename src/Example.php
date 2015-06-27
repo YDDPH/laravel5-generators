6aed05efa21a77352bc86dd010f7c59abe31ca2c
JC Ricaro <jcricaro@gmail.com>
2015-06-27 06:28:36 +0000

    Initial commit

---
+++ src/Example.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Yddph\L5-generators;
+
+class Example
+{
+
+}
---
+++ tests/ExampleTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Yddph\L5-generators\Tests;
+
+class ExampleTest extends \PHPUnit_Framework_TestCase
+{
+
+}
