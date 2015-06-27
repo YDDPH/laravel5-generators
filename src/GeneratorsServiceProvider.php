9f6a802904a7d260950c3dd6df8ba400b8d7a73c
JC Ricaro <jcricaro@gmail.com>
2015-06-27 06:38:45 +0000

    Service provider

---
+++ src/GeneratorsServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Yddph\L5;
+
+use Illuminate\Support\ServiceProvider;
+
+class GeneratorsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerResourceGenerator();
+    }
+
+    private function registerResourceGenerator()
+    {
+
+    }
+}
